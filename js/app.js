e521bf7ad3618c705c4f3c40849166a47efcc1f9
Druhin Tarafder <druhinpro@gmail.com>
2022-07-02 12:34:37 +0530

    app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,39 @@
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+/* https://github.com/grcmichael/gscroll */
+/* GSAP + ScrollTrigger + GScroll */
 
-const smoother = ScrollSmoother.create({
-  smooth: 2,
-  effects: true,
- // normalizeScroll: true,
-  smoothTouch: 0.1,
+import gsap from "https://cdn.skypack.dev/gsap"
+import ScrollTrigger from "https://cdn.skypack.dev/gsap/ScrollTrigger"
+import GScroll from "https://cdn.skypack.dev/@grcmichael/gscroll"
+
+gsap.registerPlugin(ScrollTrigger)
+
+const scroll = new GScroll(
+  '#GScroll',
+  0.6,
+  () => {ScrollTrigger.update()}
+);
+
+scroll.init();
+scroll.wheel();
+
+const scroller = document.getElementById('GScroll')
+ScrollTrigger.defaults({
+  scroller: scroller
+})
+
+ScrollTrigger.scrollerProxy(scroller, {
+  scrollTop(value) {
+    if (arguments.length) {
+      scroll.current = -value // setter
+    }
+    return -scroll.current // getter
+  },
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight}
+  }
 });
+
+
+window.addEventListener('resize', () => {
+  scroll.resize()
+})
