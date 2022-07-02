233ab5164f03fd3b2c4d305f49026cf7f317c3d5
Druhin Tarafder <druhinpro@gmail.com>
2022-07-02 12:19:47 +0530

    app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+const smoother = ScrollSmoother.create({
+  smooth: 2,
+  effects: true,
+ // normalizeScroll: true,
+  smoothTouch: 0.1,
+});
