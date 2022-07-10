7e2668b2c60cd61d8f7fb8fd0b384d17fed50202
Druhin Tarafder <druhinpro@gmail.com>
2022-07-10 19:15:48 +0530

    test

--- js/marquee.js
+++ js/marquee.js
@@ -1,169 +1,385 @@
 Marquee3k.pauseAll();
 
+
+////////////////////////////////////////////////////////////////////////////////
+
+
 $(".a0").mouseenter(function () {
 	Marquee3k.play(0);
+	for(i=0 ; i<=23 ; i++)
+	{
+		if(i!=0)
+		{
+			document.getElementById("a"+i).style.filter = "blur(10px)";
+		}
+	}
 });
 $(".a0").mouseleave(function () {
 	Marquee3k.pause(0);
+	for(i=0 ; i<=23 ; i++)
+	{
+		document.getElementById("a"+i).style.filter = "blur(0px)";
+	}
 });
+
+
+////////////////////////////////////////////////////////////////////////////////
+
 
 $(".a1").mouseenter(function () {
 	Marquee3k.play(1);
+	for(i=0 ; i<=23 ; i++)
+	{
+		if(i!=1)
+		{
+			document.getElementById("a"+i).style.filter = "blur(10px)";
+		}
+	}
 });
 $(".a1").mouseleave(function () {
 	Marquee3k.pause(1);
+	for(i=0 ; i<=23 ; i++)
+	{
+		document.getElementById("a"+i).style.filter = "blur(0px)";
+	}
 });
+
+
+////////////////////////////////////////////////////////////////////////////////
+
 
 $(".a2").mouseenter(function () {
 	Marquee3k.play(2);
+	for(i=0 ; i<=23 ; i++)
+	{
+		if(i!=2)
+		{
+			document.getElementById("a"+i).style.filter = "blur(10px)";
+		}
+	}
 });
 $(".a2").mouseleave(function () {
 	Marquee3k.pause(2);
+	for(i=0 ; i<=23 ; i++)
+	{
+		document.getElementById("a"+i).style.filter = "blur(0px)";
+	}
 });
+
+
+////////////////////////////////////////////////////////////////////////////////
+
 
 $(".a3").mouseenter(function () {
 	Marquee3k.play(3);
 });
 $(".a3").mouseleave(function () {
 	Marquee3k.pause(3);
+	for(i=0 ; i<=23 ; i++)
+	{
+		document.getElementById("a"+i).style.filter = "blur(0px)";
+	}
 });
+
+
+////////////////////////////////////////////////////////////////////////////////
+
 
 $(".a4").mouseenter(function () {
 	Marquee3k.play(4);
 });
 $(".a4").mouseleave(function () {
 	Marquee3k.pause(4);
+	for(i=0 ; i<=23 ; i++)
+	{
+		document.getElementById("a"+i).style.filter = "blur(0px)";
+	}
 });
+
+
+////////////////////////////////////////////////////////////////////////////////
+
 
 $(".a5").mouseenter(function () {
 	Marquee3k.play(5);
 });
 $(".a5").mouseleave(function () {
 	Marquee3k.pause(5);
+	for(i=0 ; i<=23 ; i++)
+	{
+		document.getElementById("a"+i).style.filter = "blur(0px)";
+	}
 });
+
+
+////////////////////////////////////////////////////////////////////////////////
+
 
 $(".a6").mouseenter(function () {
 	Marquee3k.play(6);
 });
 $(".a6").mouseleave(function () {
 	Marquee3k.pause(6);
+	for(i=0 ; i<=23 ; i++)
+	{
+		document.getElementById("a"+i).style.filter = "blur(0px)";
+	}
 });
+
+
+////////////////////////////////////////////////////////////////////////////////
+
 
 $(".a7").mouseenter(function () {
 	Marquee3k.play(7);
 });
 $(".a7").mouseleave(function () {
 	Marquee3k.pause(7);
+	for(i=0 ; i<=23 ; i++)
+	{
+		document.getElementById("a"+i).style.filter = "blur(0px)";
+	}
 });
+
+
+////////////////////////////////////////////////////////////////////////////////
+
 
 $(".a8").mouseenter(function () {
 	Marquee3k.play(8);
 });
 $(".a8").mouseleave(function () {
 	Marquee3k.pause(8);
+	for(i=0 ; i<=23 ; i++)
+	{
+		document.getElementById("a"+i).style.filter = "blur(0px)";
+	}
 });
+
+
+////////////////////////////////////////////////////////////////////////////////
+
 
 $(".a9").mouseenter(function () {
 	Marquee3k.play(9);
 });
 $(".a9").mouseleave(function () {
 	Marquee3k.pause(9);
+	for(i=0 ; i<=23 ; i++)
+	{
+		document.getElementById("a"+i).style.filter = "blur(0px)";
+	}
 });
+
+
+////////////////////////////////////////////////////////////////////////////////
+
 
 $(".a10").mouseenter(function () {
 	Marquee3k.play(10);
 });
 $(".a10").mouseleave(function () {
 	Marquee3k.pause(10);
+	for(i=0 ; i<=23 ; i++)
+	{
+		document.getElementById("a"+i).style.filter = "blur(0px)";
+	}
 });
+
+
+////////////////////////////////////////////////////////////////////////////////
+
 
 $(".a11").mouseenter(function () {
 	Marquee3k.play(11);
 });
 $(".a11").mouseleave(function () {
 	Marquee3k.pause(11);
+	for(i=0 ; i<=23 ; i++)
+	{
+		document.getElementById("a"+i).style.filter = "blur(0px)";
+	}
 });
+
+
+////////////////////////////////////////////////////////////////////////////////
+
 
 $(".a12").mouseenter(function () {
 	Marquee3k.play(12);
 });
 $(".a12").mouseleave(function () {
 	Marquee3k.pause(12);
+	for(i=0 ; i<=23 ; i++)
+	{
+		document.getElementById("a"+i).style.filter = "blur(0px)";
+	}
 });
+
+
+////////////////////////////////////////////////////////////////////////////////
+
 
 $(".a13").mouseenter(function () {
 	Marquee3k.play(13);
 });
 $(".a13").mouseleave(function () {
 	Marquee3k.pause(13);
+	for(i=0 ; i<=23 ; i++)
+	{
+		document.getElementById("a"+i).style.filter = "blur(0px)";
+	}
 });
+
+
+////////////////////////////////////////////////////////////////////////////////
+
 
 $(".a14").mouseenter(function () {
 	Marquee3k.play(14);
 });
 $(".a14").mouseleave(function () {
 	Marquee3k.pause(14);
+	for(i=0 ; i<=23 ; i++)
+	{
+		document.getElementById("a"+i).style.filter = "blur(0px)";
+	}
 });
+
+
+////////////////////////////////////////////////////////////////////////////////
+
 
 $(".a15").mouseenter(function () {
 	Marquee3k.play(15);
 });
 $(".a15").mouseleave(function () {
 	Marquee3k.pause(15);
+	for(i=0 ; i<=23 ; i++)
+	{
+		document.getElementById("a"+i).style.filter = "blur(0px)";
+	}
 });
+
+
+////////////////////////////////////////////////////////////////////////////////
+
 
 $(".a16").mouseenter(function () {
 	Marquee3k.play(16);
 });
 $(".a16").mouseleave(function () {
 	Marquee3k.pause(16);
+	for(i=0 ; i<=23 ; i++)
+	{
+		document.getElementById("a"+i).style.filter = "blur(0px)";
+	}
 });
+
+
+////////////////////////////////////////////////////////////////////////////////
+
 
 $(".a17").mouseenter(function () {
 	Marquee3k.play(17);
 });
 $(".a17").mouseleave(function () {
 	Marquee3k.pause(17);
+	for(i=0 ; i<=23 ; i++)
+	{
+		document.getElementById("a"+i).style.filter = "blur(0px)";
+	}
 });
+
+
+////////////////////////////////////////////////////////////////////////////////
+
 
 $(".a18").mouseenter(function () {
 	Marquee3k.play(18);
 });
 $(".a18").mouseleave(function () {
 	Marquee3k.pause(18);
+	for(i=0 ; i<=23 ; i++)
+	{
+		document.getElementById("a"+i).style.filter = "blur(0px)";
+	}
 });
+
+
+////////////////////////////////////////////////////////////////////////////////
+
 
 $(".a19").mouseenter(function () {
 	Marquee3k.play(19);
 });
 $(".a19").mouseleave(function () {
 	Marquee3k.pause(19);
+	for(i=0 ; i<=23 ; i++)
+	{
+		document.getElementById("a"+i).style.filter = "blur(0px)";
+	}
 });
+
+
+////////////////////////////////////////////////////////////////////////////////
+
 
 $(".a20").mouseenter(function () {
 	Marquee3k.play(20);
 });
 $(".a20").mouseleave(function () {
 	Marquee3k.pause(20);
+	for(i=0 ; i<=23 ; i++)
+	{
+		document.getElementById("a"+i).style.filter = "blur(0px)";
+	}
 });
+
+
+////////////////////////////////////////////////////////////////////////////////
+
 
 $(".a21").mouseenter(function () {
 	Marquee3k.play(21);
 });
 $(".a21").mouseleave(function () {
 	Marquee3k.pause(21);
+	for(i=0 ; i<=23 ; i++)
+	{
+		document.getElementById("a"+i).style.filter = "blur(0px)";
+	}
 });
+
+
+////////////////////////////////////////////////////////////////////////////////
+
 
 $(".a22").mouseenter(function () {
 	Marquee3k.play(22);
 });
 $(".a22").mouseleave(function () {
 	Marquee3k.pause(22);
+	for(i=0 ; i<=23 ; i++)
+	{
+		document.getElementById("a"+i).style.filter = "blur(0px)";
+	}
 });
+
+
+////////////////////////////////////////////////////////////////////////////////
+
 
 $(".a23").mouseenter(function () {
 	Marquee3k.play(23);
 });
 $(".a23").mouseleave(function () {
 	Marquee3k.pause(23);
+	for(i=0 ; i<=23 ; i++)
+	{
+		document.getElementById("a"+i).style.filter = "blur(0px)";
+	}
 });
+
+
+////////////////////////////////////////////////////////////////////////////////
