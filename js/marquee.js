ffc678b343bf7c790580a4ab596e72287c712b3e
Druhin Tarafder <druhinpro@gmail.com>
2022-07-10 18:44:09 +0530

    Create marquee.js

---
+++ js/marquee.js
@@ -0,0 +1,169 @@
+Marquee3k.pauseAll();
+
+$(".a0").mouseenter(function () {
+	Marquee3k.play(0);
+});
+$(".a0").mouseleave(function () {
+	Marquee3k.pause(0);
+});
+
+$(".a1").mouseenter(function () {
+	Marquee3k.play(1);
+});
+$(".a1").mouseleave(function () {
+	Marquee3k.pause(1);
+});
+
+$(".a2").mouseenter(function () {
+	Marquee3k.play(2);
+});
+$(".a2").mouseleave(function () {
+	Marquee3k.pause(2);
+});
+
+$(".a3").mouseenter(function () {
+	Marquee3k.play(3);
+});
+$(".a3").mouseleave(function () {
+	Marquee3k.pause(3);
+});
+
+$(".a4").mouseenter(function () {
+	Marquee3k.play(4);
+});
+$(".a4").mouseleave(function () {
+	Marquee3k.pause(4);
+});
+
+$(".a5").mouseenter(function () {
+	Marquee3k.play(5);
+});
+$(".a5").mouseleave(function () {
+	Marquee3k.pause(5);
+});
+
+$(".a6").mouseenter(function () {
+	Marquee3k.play(6);
+});
+$(".a6").mouseleave(function () {
+	Marquee3k.pause(6);
+});
+
+$(".a7").mouseenter(function () {
+	Marquee3k.play(7);
+});
+$(".a7").mouseleave(function () {
+	Marquee3k.pause(7);
+});
+
+$(".a8").mouseenter(function () {
+	Marquee3k.play(8);
+});
+$(".a8").mouseleave(function () {
+	Marquee3k.pause(8);
+});
+
+$(".a9").mouseenter(function () {
+	Marquee3k.play(9);
+});
+$(".a9").mouseleave(function () {
+	Marquee3k.pause(9);
+});
+
+$(".a10").mouseenter(function () {
+	Marquee3k.play(10);
+});
+$(".a10").mouseleave(function () {
+	Marquee3k.pause(10);
+});
+
+$(".a11").mouseenter(function () {
+	Marquee3k.play(11);
+});
+$(".a11").mouseleave(function () {
+	Marquee3k.pause(11);
+});
+
+$(".a12").mouseenter(function () {
+	Marquee3k.play(12);
+});
+$(".a12").mouseleave(function () {
+	Marquee3k.pause(12);
+});
+
+$(".a13").mouseenter(function () {
+	Marquee3k.play(13);
+});
+$(".a13").mouseleave(function () {
+	Marquee3k.pause(13);
+});
+
+$(".a14").mouseenter(function () {
+	Marquee3k.play(14);
+});
+$(".a14").mouseleave(function () {
+	Marquee3k.pause(14);
+});
+
+$(".a15").mouseenter(function () {
+	Marquee3k.play(15);
+});
+$(".a15").mouseleave(function () {
+	Marquee3k.pause(15);
+});
+
+$(".a16").mouseenter(function () {
+	Marquee3k.play(16);
+});
+$(".a16").mouseleave(function () {
+	Marquee3k.pause(16);
+});
+
+$(".a17").mouseenter(function () {
+	Marquee3k.play(17);
+});
+$(".a17").mouseleave(function () {
+	Marquee3k.pause(17);
+});
+
+$(".a18").mouseenter(function () {
+	Marquee3k.play(18);
+});
+$(".a18").mouseleave(function () {
+	Marquee3k.pause(18);
+});
+
+$(".a19").mouseenter(function () {
+	Marquee3k.play(19);
+});
+$(".a19").mouseleave(function () {
+	Marquee3k.pause(19);
+});
+
+$(".a20").mouseenter(function () {
+	Marquee3k.play(20);
+});
+$(".a20").mouseleave(function () {
+	Marquee3k.pause(20);
+});
+
+$(".a21").mouseenter(function () {
+	Marquee3k.play(21);
+});
+$(".a21").mouseleave(function () {
+	Marquee3k.pause(21);
+});
+
+$(".a22").mouseenter(function () {
+	Marquee3k.play(22);
+});
+$(".a22").mouseleave(function () {
+	Marquee3k.pause(22);
+});
+
+$(".a23").mouseenter(function () {
+	Marquee3k.play(23);
+});
+$(".a23").mouseleave(function () {
+	Marquee3k.pause(23);
+});
