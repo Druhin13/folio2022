ae9c190e90c2f6dca213456df7137a010b464a49
Druhin Tarafder <druhinpro@gmail.com>
2022-07-14 11:12:37 +0530

    combine

--- js/loader.js
+++ js/loader.js
@@ -1,3 +1,29 @@
+//jQuerySimpleCounter.js
+(function ($) {
+  $.fn.jQuerySimpleCounter = function (options) {
+    let settings = $.extend({
+      start: 0,
+      end: 100,
+      easing: 'swing',
+      duration: 400,
+      complete: ''
+    }, options);
+
+    const thisElement = $(this);
+
+    $({ count: settings.start }).animate({ count: settings.end }, {
+      duration: settings.duration,
+      easing: settings.easing,
+      step: function () {
+        let mathCount = Math.ceil(this.count);
+        thisElement.text(mathCount);
+      },
+      complete: settings.complete
+    });
+  };
+}(jQuery));
+
+//locomotive initialization
 const locoScroll = new LocomotiveScroll({
   el: document.querySelector(".locomotive-scroll"),
   smooth: true,
@@ -10,9 +36,7 @@ function remove() {
   $(".w-webflow-badge").remove();
 }
 
-
-
-
+//counter initialization
 setTimeout(() => {
   $('#count').jQuerySimpleCounter({
     start: 0,
@@ -22,14 +46,12 @@ setTimeout(() => {
   });
 }, 2000);
 
-
-
+//timer/checker initialization
 let test_timer = setInterval(test, 10);
 function test() {
   if (parseInt($("#count")[0].innerHTML) > 8) {
     $('#zero').css('display', 'none');
   }
-
 
   if ($("#count")[0].innerHTML == '100') {
     //removing the badge
@@ -42,11 +64,10 @@ function test() {
     //setting the canvas pointer events to none, so that user cannot interact with the canvas once the loading animation is done
     $('#canvas-div').css('pointer-events', 'none');
 
-
     //animate the trigger
     $(".trigger1").click();
 
-    // waiting for 1.75 seconds and then calculating the new page height
+    //waiting for 1.75 seconds and then calculating the new page height
     setTimeout(() => {
       locoScroll.update();
       locoScroll.start();
@@ -56,8 +77,7 @@ function test() {
 
 
 
-//page link scroll triggers
-
+//page link scroll trigger initialization
 const about = document.querySelector('#about');
 const work = document.querySelector('#work');
 const awards = document.querySelector('#awards');
