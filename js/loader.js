bf617b9281b8533d749be702e3b619bebe7d8d06
Druhin Tarafder <druhinpro@gmail.com>
2022-07-17 01:09:19 +0530

    test

--- js/loader.js
+++ js/loader.js
@@ -83,7 +83,7 @@ setTimeout(() => {
   }
   function draw(event) {
     ctx.beginPath();
-    ctx.lineWidth = 1.25;
+    ctx.lineWidth = 2.5;
     ctx.lineCap = "round";
     ctx.strokeStyle = "#fff";
     ctx.moveTo(coord.x, coord.y);
