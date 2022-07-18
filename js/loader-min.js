f8b0d5836f40303c8dc2eaf4d82ad4563c880627
Druhin Tarafder <druhinpro@gmail.com>
2022-07-18 23:19:23 +0530

    test

--- js/loader-min.js
+++ js/loader-min.js
@@ -1,220 +1,240 @@
 //jQuerySimpleCounter.js
 (function ($) {
-    $.fn.jQuerySimpleCounter = function (options) {
-      let settings = $.extend({
-        start: 0,
-        end: 100,
-        easing: 'swing',
-        duration: 400,
-        complete: ''
-      }, options);
-  
-      const thisElement = $(this);
-  
-      $({ count: settings.start }).animate({ count: settings.end }, {
-        duration: settings.duration,
-        easing: settings.easing,
-        step: function () {
-          let mathCount = Math.ceil(this.count);
-          thisElement.text(mathCount);
-        },
-        complete: settings.complete
-      });
-    };
-  }(jQuery));
-  
-  //locomotive initialization
-  const locoScroll = new LocomotiveScroll({
-    el: document.querySelector(".locomotive-scroll"),
-    smooth: true,
-    multiplier: 0.35,
-  });
-  locoScroll.stop();
-  
-  let timer = setInterval(remove, 10);
-  function remove() {
-    $(".w-webflow-badge").remove();
-  }
-  
-  //counter initialization
-  setTimeout(() => {
-  
-    var new_canvas = document.createElement('canvas');
-    new_canvas.id = "sketch";
-    new_canvas.className = "sketch";
-    new_canvas.style.position = "absolute";
-    new_canvas.style.zIndex = "100";
-    new_canvas.style.backgroundColor = "#ffffff00";
-    //new_canvas.style.pointerEvents = "none";
-    new_canvas.style.marginLeft = "52vw";
-  
-    //document.body.appendChild(new_canvas);
-    document.body.insertBefore(new_canvas, document.body.firstChild);
-  
-    const canvas = document.getElementById("sketch");
-    const ctx = canvas.getContext("2d");
-  
-    //testing size
-    canvas.width = parent.offsetWidth;
-    canvas.height = parent.offsetHeight;
-    ctx.canvas.width = parent.offsetWidth;
-    ctx.canvas.height = parent.offsetHeight;
-  
-    let coord = { x: 0, y: 0 };
-  
-    document.addEventListener("mouseover", start);
-    window.addEventListener("resize", resize);
-  
-    resize();
-  
-    function resize() {
-      ctx.canvas.width = window.innerWidth;
-      ctx.canvas.height = window.innerHeight;
-    }
-    function reposition(event) {
-      coord.x = event.clientX - canvas.offsetLeft;
-      coord.y = event.clientY - canvas.offsetTop;
-    }
-    function start(event) {
-      document.addEventListener("mousemove", draw);
-      reposition(event);
-    }
-    function stop() {
-      document.removeEventListener("mousemove", draw);
-    }
-    function draw(event) {
-      ctx.beginPath();
-      ctx.lineWidth = 1.15;
-      ctx.lineCap = "round";
-      ctx.strokeStyle = "#A4A4A4";
-      ctx.moveTo(coord.x, coord.y);
-      reposition(event);
-      ctx.lineTo(coord.x, coord.y);
-      ctx.stroke();
-    }
-  
-  
-    $('#count').jQuerySimpleCounter({
+  $.fn.jQuerySimpleCounter = function (options) {
+    let settings = $.extend({
       start: 0,
       end: 100,
       easing: 'swing',
-      duration: 1000, //set the duration of the loading animation (5000 - 7000 ~6000)
+      duration: 400,
+      complete: ''
+    }, options);
+
+    const thisElement = $(this);
+
+    $({ count: settings.start }).animate({ count: settings.end }, {
+      duration: settings.duration,
+      easing: settings.easing,
+      step: function () {
+        let mathCount = Math.ceil(this.count);
+        thisElement.text(mathCount);
+      },
+      complete: settings.complete
     });
-  
-  }, 2000);
-  
-  //timer/checker initialization
-  let test_timer = setInterval(test, 10);
-  function test() {
-    if (parseInt($("#count")[0].innerHTML) > 8) {
-      $('#zero').css('display', 'none');
-    }
-  
-    if ($("#count")[0].innerHTML == '100') {
-      //removing the badge
-      $(".w-webflow-badge").remove();
-      //turning off the continuous function run
-      clearInterval(timer);
-      clearInterval(test_timer);
-      //when loading is finished, setting cursor back to default...
-      document.body.style.cursor = 'default';
-      //setting the canvas pointer events to none, so that user cannot interact with the canvas once the loading animation is done
-      $('#canvas-div').css('pointer-events', 'none');
-  
-      //animate the trigger
-      $(".trigger1").click();
-  
-  
-      //waiting for 1 second, and then hiding the sketch canvas
-      setTimeout(() => {
-        //hiding the sketch canvas
-        document.getElementById("sketch").style.display = "none";
-      }, 1000);
-  
-      //waiting for 1.75 seconds and then calculating the new page height
-      setTimeout(() => {
-        locoScroll.update();
-        locoScroll.start();
-  
-        //loading the case study preview gifs
-        $('head').append('<link rel="stylesheet" href="https://druh.in/folio2022/css/preview.css"/>');
-      }, 750);
-    }
+  };
+}(jQuery));
+
+//locomotive initialization
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector(".locomotive-scroll"),
+  smooth: true,
+  multiplier: 0.35,
+});
+locoScroll.stop();
+
+let timer = setInterval(remove, 10);
+function remove() {
+  $(".w-webflow-badge").remove();
+}
+
+
+//hiding the 'something' section
+$('#something').hide();
+
+
+//counter initialization
+setTimeout(() => {
+
+  var new_canvas = document.createElement('canvas');
+  new_canvas.id = "sketch";
+  new_canvas.className = "sketch";
+  new_canvas.style.position = "absolute";
+  new_canvas.style.zIndex = "100";
+  new_canvas.style.backgroundColor = "#ffffff00";
+  //new_canvas.style.pointerEvents = "none";
+  new_canvas.style.marginLeft = "52vw";
+
+  //document.body.appendChild(new_canvas);
+  document.body.insertBefore(new_canvas, document.body.firstChild);
+
+  const canvas = document.getElementById("sketch");
+  const ctx = canvas.getContext("2d");
+
+  //testing size
+  canvas.width = parent.offsetWidth;
+  canvas.height = parent.offsetHeight;
+  ctx.canvas.width = parent.offsetWidth;
+  ctx.canvas.height = parent.offsetHeight;
+
+  let coord = { x: 0, y: 0 };
+
+  document.addEventListener("mouseover", start);
+  window.addEventListener("resize", resize);
+
+  resize();
+
+  function resize() {
+    ctx.canvas.width = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
   }
-  
-  
-  
-  //page link scroll trigger initialization
-  const about = document.querySelector('#about');
-  const work = document.querySelector('#work');
-  const awards = document.querySelector('#awards');
-  const testimonials = document.querySelector('#testimonials');
-  const contact = document.querySelector('#contact');
-  
-  
-  $('#about-link').click(function () {
-    locoScroll.scrollTo(about);
-  });
-  
-  $('#work-link').click(function () {
-    locoScroll.scrollTo(work);
-  });
-  
-  $('#awards-link').click(function () {
-    locoScroll.scrollTo(awards);
-  });
-  
-  $('#testimonials-link').click(function () {
-    locoScroll.scrollTo(testimonials);
-  });
-  
-  $('#contact-link').click(function () {
-    locoScroll.scrollTo(contact);
-  });
-  
-  
-  
-  
-  /* testing on work section - case study tabs 
-  
-  const h1 = document.querySelectorAll(".hashtag-txt");
-  const h2 = document.querySelectorAll(".hashtag-txt-hidden");
-  
-  for (i = 0; i < h1.length; i++) {
-    nh1 = h1[i].textContent.replace("#", "");
-    h1[i].innerText = nh1;
+  function reposition(event) {
+    coord.x = event.clientX - canvas.offsetLeft;
+    coord.y = event.clientY - canvas.offsetTop;
   }
-  
-  for (j = 0; j < h2.length; j++) {
-    nh2 = h2[j].textContent.replace("#", "");
-    h2[j].innerText = nh2;
+  function start(event) {
+    document.addEventListener("mousemove", draw);
+    reposition(event);
   }
-  
-  */
-  
-  
-  
-  /* time calc */
-  
-  let options = {
-    timeZone: 'Europe/London',
-    //year: 'numeric',
-    //month: 'numeric',
-    //day: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit',
-    //second: 'numeric',
-  },
-    formatter = new Intl.DateTimeFormat([], options);
-  
-  let time_a = formatter.format(new Date());
-  
-  let ctime = setInterval(timecalc, 1000);
-  function timecalc() {
-    let time_b = formatter.format(new Date());
-    $("#time").text(time_b);
-    if (time_b != time_a) {
-      time_a = time_b;
-      $(".circle").click();
-    }
+  function stop() {
+    document.removeEventListener("mousemove", draw);
   }
+  function draw(event) {
+    ctx.beginPath();
+    ctx.lineWidth = 1.15;
+    ctx.lineCap = "round";
+    ctx.strokeStyle = "#A4A4A4";
+    ctx.moveTo(coord.x, coord.y);
+    reposition(event);
+    ctx.lineTo(coord.x, coord.y);
+    ctx.stroke();
+  }
+
+
+  $('#count').jQuerySimpleCounter({
+    start: 0,
+    end: 100,
+    easing: 'swing',
+    duration: 1000, //set the duration of the loading animation (5000 - 7000 ~6000)
+  });
+
+}, 2000);
+
+//timer/checker initialization
+let test_timer = setInterval(test, 10);
+function test() {
+  if (parseInt($("#count")[0].innerHTML) > 8) {
+    $('#zero').css('display', 'none');
+  }
+
+  if ($("#count")[0].innerHTML == '100') {
+    //removing the badge
+    $(".w-webflow-badge").remove();
+    //turning off the continuous function run
+    clearInterval(timer);
+    clearInterval(test_timer);
+    //when loading is finished, setting cursor back to default...
+    document.body.style.cursor = 'default';
+    //setting the canvas pointer events to none, so that user cannot interact with the canvas once the loading animation is done
+    $('#canvas-div').css('pointer-events', 'none');
+
+    //animate the trigger
+    $(".trigger1").click();
+
+
+    //waiting for 1 second, and then hiding the sketch canvas
+    setTimeout(() => {
+      //hiding the sketch canvas
+      document.getElementById("sketch").style.display = "none";
+    }, 1000);
+
+    //waiting for 1.75 seconds and then calculating the new page height
+    setTimeout(() => {
+      locoScroll.update();
+      locoScroll.start();
+
+      //loading the case study preview gifs
+      $('head').append('<link rel="stylesheet" href="https://druh.in/folio2022/css/preview.css"/>');
+    }, 750);
+  }
+}
+
+
+
+//page link scroll trigger initialization
+const about = document.querySelector('#about');
+const work = document.querySelector('#work');
+const awards = document.querySelector('#awards');
+const testimonials = document.querySelector('#testimonials');
+const contact = document.querySelector('#contact');
+
+
+$('#about-link').click(function () {
+  locoScroll.scrollTo(about);
+});
+
+$('#work-link').click(function () {
+  locoScroll.scrollTo(work);
+});
+
+$('#awards-link').click(function () {
+  locoScroll.scrollTo(awards);
+});
+
+$('#testimonials-link').click(function () {
+  locoScroll.scrollTo(testimonials);
+});
+
+$('#contact-link').click(function () {
+  locoScroll.scrollTo(contact);
+});
+
+
+
+
+/* testing on work section - case study tabs 
+
+const h1 = document.querySelectorAll(".hashtag-txt");
+const h2 = document.querySelectorAll(".hashtag-txt-hidden");
+
+for (i = 0; i < h1.length; i++) {
+  nh1 = h1[i].textContent.replace("#", "");
+  h1[i].innerText = nh1;
+}
+
+for (j = 0; j < h2.length; j++) {
+  nh2 = h2[j].textContent.replace("#", "");
+  h2[j].innerText = nh2;
+}
+
+*/
+
+
+
+/* time calc */
+
+let options = {
+  timeZone: 'Europe/London',
+  //year: 'numeric',
+  //month: 'numeric',
+  //day: 'numeric',
+  hour: '2-digit',
+  minute: '2-digit',
+  //second: 'numeric',
+},
+  formatter = new Intl.DateTimeFormat([], options);
+
+let time_a = formatter.format(new Date());
+
+let ctime = setInterval(timecalc, 1000);
+function timecalc() {
+  let time_b = formatter.format(new Date());
+  $("#time").text(time_b);
+  if (time_b != time_a) {
+    time_a = time_b;
+    $(".circle").click();
+  }
+}
+
+
+
+//if user wanna see more
+
+$('#something-trigger').click(function () {
+  locoScroll.stop();
+  $('#something').show();
+
+  setTimeout(() => {
+    locoScroll.update();
+    locoScroll.start();
+  }, 250);
+
+});
