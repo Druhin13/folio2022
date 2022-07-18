82038066ad10165b3cc72673967d6ded80a84486
Druhin Tarafder <druhinpro@gmail.com>
2022-07-19 01:10:55 +0530

    test

--- js/loader.js
+++ js/loader.js
@@ -37,9 +37,12 @@ function remove() {
 }
 
 
+/*
+
 //hiding the 'something' section
 $('#something').hide();
 
+*/
 
 //counter initialization
 setTimeout(() => {
@@ -107,6 +110,19 @@ setTimeout(() => {
   });
 
 }, 2000);
+
+
+
+
+
+// jquery inview
+
+!function(a){"function"==typeof define&&define.amd?define(["jquery"],a):"object"==typeof exports?module.exports=a(require("jquery")):a(jQuery)}(function(a){function i(){var b,c,d={height:f.innerHeight,width:f.innerWidth};return d.height||(b=e.compatMode,(b||!a.support.boxModel)&&(c="CSS1Compat"===b?g:e.body,d={height:c.clientHeight,width:c.clientWidth})),d}function j(){return{top:f.pageYOffset||g.scrollTop||e.body.scrollTop,left:f.pageXOffset||g.scrollLeft||e.body.scrollLeft}}function k(){if(b.length){var e=0,f=a.map(b,function(a){var b=a.data.selector,c=a.$element;return b?c.find(b):c});for(c=c||i(),d=d||j();e<b.length;e++)if(a.contains(g,f[e][0])){var h=a(f[e]),k={height:h[0].offsetHeight,width:h[0].offsetWidth},l=h.offset(),m=h.data("inview");if(!d||!c)return;l.top+k.height>d.top&&l.top<d.top+c.height&&l.left+k.width>d.left&&l.left<d.left+c.width?m||h.data("inview",!0).trigger("inview",[!0]):m&&h.data("inview",!1).trigger("inview",[!1])}}}var c,d,h,b=[],e=document,f=window,g=e.documentElement;a.event.special.inview={add:function(c){b.push({data:c,$element:a(this),element:this}),!h&&b.length&&(h=setInterval(k,250))},remove:function(a){for(var c=0;c<b.length;c++){var d=b[c];if(d.element===this&&d.data.guid===a.guid){b.splice(c,1);break}}b.length||(clearInterval(h),h=null)}},a(f).bind("scroll resize scrollstop",function(){c=d=null}),!g.addEventListener&&g.attachEvent&&g.attachEvent("onfocusin",function(){d=null})});
+
+
+
+
+
 
 //timer/checker initialization
 let test_timer = setInterval(test, 10);
@@ -225,6 +241,7 @@ function timecalc() {
 }
 
 
+/*
 
 //if user wanna see more
 
@@ -232,16 +249,21 @@ $('#something-trigger').click(function () {
   $('#something').show();
   locoScroll.stop();
 
-
-  //new locomotive initialization (when users wanna see more)
-  const locoScroll2 = new LocomotiveScroll({
-    el: document.querySelector(".locomotive-scroll"),
-    smooth: true,
-    multiplier: 0.35,
-  });
-
   setTimeout(() => {
-    locoScroll2.start();
-  }, 350);
+    locoScroll.update();
+  }, 250);
+  setTimeout(() => {
+    locoScroll.start();
+  }, 250);
 
+});
+
+*/
+
+
+
+$('#something').on('inview', function (event, isInView) {
+  if (isInView) {
+    locoScroll.stop();
+  } else { }
 });
