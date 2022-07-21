1024643254d782a35f72a52c7929e3314e20227b
Druhin Tarafder <druhinpro@gmail.com>
2022-07-21 06:03:20 +0530

    test

--- js/loader.js
+++ js/loader.js
@@ -645,18 +645,19 @@ $('#menu').on('mousedown', function () {
   if ($("#menu").position().top == 0) {
     $('#menu-demo1').css("opacity", "0%");
     $('#menu-demo2').css("opacity", "75%");
-    $('#menu-demo2').css("cursor", "grabbing");
-    $('#menu-demo2').css("cursor", "-webkit-grabbing");
+    $('#menu').css("cursor", "grabbing");
+    $('#menu').css("cursor", "-webkit-grabbing");
   }
   else {
     $('#menu-demo1').css("opacity", "75%");
     $('#menu-demo2').css("opacity", "0%");
-    $('#menu-demo2').css("cursor", "-webkit-grabbing");
+    $('#menu').css("cursor", "grabbing");
+    $('#menu').css("cursor", "-webkit-grabbing");
   }
 
 }
 ).on('mouseup mouseleave', function () {
   $('#menu-demo1').css("opacity", "0%");
   $('#menu-demo2').css("opacity", "0%");
-  $('#menu-demo2').css("cursor", "auto");
+  $('#menu').css("cursor", "grab");
 });
