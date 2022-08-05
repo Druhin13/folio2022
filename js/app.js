4a96a3c0607646e7b86b3c610cccb0fe77deaf8f
Druhin Tarafder <druhinpro@gmail.com>
2022-08-05 22:00:22 +0530

    test

--- js/app.js
+++ js/app.js
@@ -27,7 +27,7 @@
 const locoScroll = new LocomotiveScroll({
   el: document.querySelector(".locomotive-scroll"),
   smooth: true,
-  multiplier: 0.25,
+  multiplier: 0.35,
 });
 locoScroll.stop();
 
