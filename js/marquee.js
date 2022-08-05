279461c41da4ae6990e0ce9c97b60cb571657b3a
Druhin Tarafder <druhinpro@gmail.com>
2022-08-06 03:23:46 +0530

    test

--- js/marquee.js
+++ js/marquee.js
@@ -207,169 +207,169 @@ Marquee3k.init()
 Marquee3k.pauseAll(), $(".a0").mouseenter(function () {
     for (Marquee3k.play(0), i = 0; 23 >= i; i++)
         if (0 != i)
-            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(13px)"
+            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0.65vw)"
 }), $(".a0").mouseleave(function () {
     for (Marquee3k.pause(0), i = 0; 23 >= i; i++)
-        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0px)"
+        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0vw)"
 }), $(".a1").mouseenter(function () {
     for (Marquee3k.play(1), i = 0; 23 >= i; i++)
         if (1 != i)
-            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(13px)"
+            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0.65vw)"
 }), $(".a1").mouseleave(function () {
     for (Marquee3k.pause(1), i = 0; 23 >= i; i++)
-        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0px)"
+        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0vw)"
 }), $(".a2").mouseenter(function () {
     for (Marquee3k.play(2), i = 0; 23 >= i; i++)
         if (2 != i)
-            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(13px)"
+            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0.65vw)"
 }), $(".a2").mouseleave(function () {
     for (Marquee3k.pause(2), i = 0; 23 >= i; i++)
-        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0px)"
+        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0vw)"
 }), $(".a3").mouseenter(function () {
     for (Marquee3k.play(3), i = 0; 23 >= i; i++)
         if (3 != i)
-            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(13px)"
+            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0.65vw)"
 }), $(".a3").mouseleave(function () {
     for (Marquee3k.pause(3), i = 0; 23 >= i; i++)
-        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0px)"
+        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0vw)"
 }), $(".a4").mouseenter(function () {
     for (Marquee3k.play(4), i = 0; 23 >= i; i++)
         if (4 != i)
-            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(13px)"
+            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0.65vw)"
 }), $(".a4").mouseleave(function () {
     for (Marquee3k.pause(4), i = 0; 23 >= i; i++)
-        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0px)"
+        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0vw)"
 }), $(".a5").mouseenter(function () {
     for (Marquee3k.play(5), i = 0; 23 >= i; i++)
         if (5 != i)
-            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(13px)"
+            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0.65vw)"
 }), $(".a5").mouseleave(function () {
     for (Marquee3k.pause(5), i = 0; 23 >= i; i++)
-        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0px)"
+        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0vw)"
 }), $(".a6").mouseenter(function () {
     for (Marquee3k.play(6), i = 0; 23 >= i; i++)
         if (6 != i)
-            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(13px)"
+            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0.65vw)"
 }), $(".a6").mouseleave(function () {
     for (Marquee3k.pause(6), i = 0; 23 >= i; i++)
-        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0px)"
+        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0vw)"
 }), $(".a7").mouseenter(function () {
     for (Marquee3k.play(7), i = 0; 23 >= i; i++)
         if (7 != i)
-            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(13px)"
+            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0.65vw)"
 }), $(".a7").mouseleave(function () {
     for (Marquee3k.pause(7), i = 0; 23 >= i; i++)
-        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0px)"
+        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0vw)"
 }), $(".a8").mouseenter(function () {
     for (Marquee3k.play(8), i = 0; 23 >= i; i++)
         if (8 != i)
-            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(13px)"
+            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0.65vw)"
 }), $(".a8").mouseleave(function () {
     for (Marquee3k.pause(8), i = 0; 23 >= i; i++)
-        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0px)"
+        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0vw)"
 }), $(".a9").mouseenter(function () {
     for (Marquee3k.play(9), i = 0; 23 >= i; i++)
         if (9 != i)
-            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(13px)"
+            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0.65vw)"
 }), $(".a9").mouseleave(function () {
     for (Marquee3k.pause(9), i = 0; 23 >= i; i++)
-        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0px)"
+        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0vw)"
 }), $(".a10").mouseenter(function () {
     for (Marquee3k.play(10), i = 0; 23 >= i; i++)
         if (10 != i)
-            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(13px)"
+            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0.65vw)"
 }), $(".a10").mouseleave(function () {
     for (Marquee3k.pause(10), i = 0; 23 >= i; i++)
-        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0px)"
+        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0vw)"
 }), $(".a11").mouseenter(function () {
     for (Marquee3k.play(11), i = 0; 23 >= i; i++)
         if (11 != i)
-            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(13px)"
+            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0.65vw)"
 }), $(".a11").mouseleave(function () {
     for (Marquee3k.pause(11), i = 0; 23 >= i; i++)
-        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0px)"
+        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0vw)"
 }), $(".a12").mouseenter(function () {
     for (Marquee3k.play(12), i = 0; 23 >= i; i++)
         if (12 != i)
-            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(13px)"
+            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0.65vw)"
 }), $(".a12").mouseleave(function () {
     for (Marquee3k.pause(12), i = 0; 23 >= i; i++)
-        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0px)"
+        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0vw)"
 }), $(".a13").mouseenter(function () {
     for (Marquee3k.play(13), i = 0; 23 >= i; i++)
         if (13 != i)
-            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(13px)"
+            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0.65vw)"
 }), $(".a13").mouseleave(function () {
     for (Marquee3k.pause(13), i = 0; 23 >= i; i++)
-        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0px)"
+        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0vw)"
 }), $(".a14").mouseenter(function () {
     for (Marquee3k.play(14), i = 0; 23 >= i; i++)
         if (14 != i)
-            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(13px)"
+            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0.65vw)"
 }), $(".a14").mouseleave(function () {
     for (Marquee3k.pause(14), i = 0; 23 >= i; i++)
-        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0px)"
+        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0vw)"
 }), $(".a15").mouseenter(function () {
     for (Marquee3k.play(15), i = 0; 23 >= i; i++)
         if (15 != i)
-            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(13px)"
+            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0.65vw)"
 }), $(".a15").mouseleave(function () {
     for (Marquee3k.pause(15), i = 0; 23 >= i; i++)
-        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0px)"
+        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0vw)"
 }), $(".a16").mouseenter(function () {
     for (Marquee3k.play(16), i = 0; 23 >= i; i++)
         if (16 != i)
-            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(13px)"
+            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0.65vw)"
 }), $(".a16").mouseleave(function () {
     for (Marquee3k.pause(16), i = 0; 23 >= i; i++)
-        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0px)"
+        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0vw)"
 }), $(".a17").mouseenter(function () {
     for (Marquee3k.play(17), i = 0; 23 >= i; i++)
         if (17 != i)
-            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(13px)"
+            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0.65vw)"
 }), $(".a17").mouseleave(function () {
     for (Marquee3k.pause(17), i = 0; 23 >= i; i++)
-        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0px)"
+        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0vw)"
 }), $(".a18").mouseenter(function () {
     for (Marquee3k.play(18), i = 0; 23 >= i; i++)
         if (18 != i)
-            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(13px)"
+            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0.65vw)"
 }), $(".a18").mouseleave(function () {
     for (Marquee3k.pause(18), i = 0; 23 >= i; i++)
-        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0px)"
+        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0vw)"
 }), $(".a19").mouseenter(function () {
     for (Marquee3k.play(19), i = 0; 23 >= i; i++)
         if (19 != i)
-            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(13px)"
+            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0.65vw)"
 }), $(".a19").mouseleave(function () {
     for (Marquee3k.pause(19), i = 0; 23 >= i; i++)
-        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0px)"
+        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0vw)"
 }), $(".a20").mouseenter(function () {
     for (Marquee3k.play(20), i = 0; 23 >= i; i++)
         if (20 != i)
-            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(13px)"
+            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0.65vw)"
 }), $(".a20").mouseleave(function () {
     for (Marquee3k.pause(20), i = 0; 23 >= i; i++)
-        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0px)"
+        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0vw)"
 }), $(".a21").mouseenter(function () {
     for (Marquee3k.play(21), i = 0; 23 >= i; i++)
         if (21 != i)
-            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(13px)"
+            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0.65vw)"
 }), $(".a21").mouseleave(function () {
     for (Marquee3k.pause(21), i = 0; 23 >= i; i++)
-        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0px)"
+        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0vw)"
 }), $(".a22").mouseenter(function () {
     for (Marquee3k.play(22), i = 0; 23 >= i; i++)
         if (22 != i)
-            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(13px)"
+            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0.65vw)"
 }), $(".a22").mouseleave(function () {
     for (Marquee3k.pause(22), i = 0; 23 >= i; i++)
-        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0px)"
+        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0vw)"
 }), $(".a23").mouseenter(function () {
     for (Marquee3k.play(23), i = 0; 23 >= i; i++)
         if (23 != i)
-            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(13px)"
+            for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0.65vw)"
 }), $(".a23").mouseleave(function () {
     for (Marquee3k.pause(23), i = 0; 23 >= i; i++)
-        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0px)"
+        for (l = document.querySelectorAll(".a" + i).length, j = 0; j < l; j++) document.querySelectorAll(".a" + i)[j].style.filter = "blur(0vw)"
 });
